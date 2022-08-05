cd3a7da5d2e47418e2b86b2aed833d418a171605
namvh21 <namvh21@viettel.com.vn>
2022-08-05 17:45:35 +0700

    feat: add break point for mobile

--- src/app/screens/header/header.component.ts
+++ src/app/screens/header/header.component.ts
@@ -1,11 +1,24 @@
+/* eslint-disable no-unused-vars */
 import { Component, OnInit } from '@angular/core';
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { distinctUntilChanged, tap } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less'],
 })
 export class HeaderComponent implements OnInit {
+  readonly breakpoint$ = this.breakpointObserver
+    .observe([
+      Breakpoints.Large,
+      Breakpoints.Medium,
+      Breakpoints.Small,
+      '(min-width: 500px)',
+    ])
+    .pipe(
+      tap((value) => console.log(value)),
+      distinctUntilChanged()
+    );
   activeItemIndex = 0;
   readonly tabs = [
     {
@@ -17,9 +30,22 @@ export class HeaderComponent implements OnInit {
       link: '/second-component',
     },
   ];
-  constructor() {}
+  currentBreakpoint: string = '';
+  Breakpoints = Breakpoints;
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
   ngOnInit(): void {
-    console.log('init header');
+    this.breakpoint$.subscribe(() => this.breakpointChanged());
+  }
+  private breakpointChanged() {
+    if (this.breakpointObserver.isMatched(Breakpoints.Large)) {
+      this.currentBreakpoint = Breakpoints.Large;
+    } else if (this.breakpointObserver.isMatched(Breakpoints.Medium)) {
+      this.currentBreakpoint = Breakpoints.Medium;
+    } else if (this.breakpointObserver.isMatched(Breakpoints.Small)) {
+      this.currentBreakpoint = Breakpoints.Small;
+    } else if (this.breakpointObserver.isMatched('(min-width: 500px)')) {
+      this.currentBreakpoint = '(min-width: 500px)';
+    }
   }
 }
